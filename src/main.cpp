046c30caf1e28d38e1bc478a5df0cfdca7ba3ab7
akhanov <andrey.khanov@calix.com>
2015-11-27 21:39:58 -0800

    CLion Commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by akhanov on 11/27/2015.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "Hello, World!!!" << endl;
+
+    return 0;
+}
